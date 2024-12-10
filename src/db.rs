032cb3539b36f58b65035de453d76d9855a8f432
SATHVIK SVS <svssathvik77@gmail.com>
2024-12-10 21:16:56 +0530

    setup mongodb

--- src/db.rs
+++ src/db.rs
@@ -1,2 +1,28 @@
-pub mod database;
+use std::env;
+
+use dotenv::dotenv;
+use mongodb::{Client, Collection};
+use reg_state::RegState;
+
 pub mod reg_state;
+
+
+
+pub struct DB{
+    pub reg_states: Collection<RegState>
+}
+
+impl DB{
+    pub async fn init() -> Self{
+        dotenv().ok();
+        let mongo_uri = env::var("DB_URL").expect("No var named DB_URL found!");
+        let client = Client::with_uri_str(mongo_uri).await.expect("Failed connecting to the database");
+        println!("Connected to database!");
+        let database = client.database("polling-app");
+        let reg_state_collection = database.collection("reg_states");
+
+        DB{
+            reg_states: reg_state_collection
+        }
+    }
+}
--- src/db/database.rs
+++
@@ -1,7 +0,0 @@
-use mongodb::Collection;
-
-use super::reg_state::RegState;
-
-pub struct DB{
-    pub reg_states: Collection<RegState>
-}
