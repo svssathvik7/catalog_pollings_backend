bc56db06e241660aeee1c21a466f2ee1787bb4a5
SATHVIK SVS <svssathvik77@gmail.com>
2024-12-10 21:36:19 +0530

    add users repo

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,16 @@
 use std::env;
 
 use dotenv::dotenv;
-use mongodb::{Client, Collection};
-use reg_state::{RegState, RegStateRepo};
+use mongodb::Client;
+use reg_state_repo::RegStateRepo;
+use users_repo::UserRepo;
 
-pub mod reg_state;
+pub mod reg_state_repo;
+pub mod users_repo;
 
 pub struct DB{
-    pub reg_states: RegStateRepo
+    pub reg_states: RegStateRepo,
+    pub users: UserRepo
 }
 
 impl DB{
@@ -18,9 +21,10 @@ impl DB{
         println!("Connected to database!");
         let database = client.database("polling-app");
         let reg_state_collection = RegStateRepo::init(&database).await;
-
+        let users_collection = UserRepo::init(&database).await;
         DB{
-            reg_states: reg_state_collection
+            reg_states: reg_state_collection,
+            users: users_collection
         }
     }
 }
