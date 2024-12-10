d10c533df4e3aa3f3b28df4bbff5adf7a0d396ec
SATHVIK SVS <svssathvik77@gmail.com>
2024-12-10 18:19:45 +0530

    init actix web

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{App, HttpResponse, HttpServer};
+
+#[actix_web::get("/")]
+async fn home() -> HttpResponse{
+    HttpResponse::Ok().json("Welcome to polling app backend")
+}
+
+#[actix_web::main]
+async fn main() -> Result<(),std::io::Error>{
+    HttpServer::new(
+        ||{
+            App::new().service(home)
+        }
+    ).bind("localhost:5000")?.run().await
+}
