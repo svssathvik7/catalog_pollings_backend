bb2f927a2f4cb92b9d2524fd6ae8f5637e0f9bd3
SATHVIK SVS <svssathvik77@gmail.com>
2024-12-16 21:48:11 +0530

    add get poll by id controller

--- src/db/polls_repo.rs
+++ src/db/polls_repo.rs
@@ -1,4 +1,5 @@
-use mongodb::{bson::oid::ObjectId, results::InsertOneResult, Collection, Database};
+use futures::TryStreamExt;
+use mongodb::{bson::{doc, oid::ObjectId, Document}, results::InsertOneResult, Collection, Database};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -23,5 +24,32 @@ impl PollRepo {
     pub async fn insert(&self, new_poll: Poll) -> Result<InsertOneResult, mongodb::error::Error> {
         let result = self.collection.insert_one(new_poll).await;
         result
+    }
+
+    pub async fn get(&self,poll_id: ObjectId) -> Result<Option<Document>,mongodb::error::Error>{
+        let pipeline = vec![
+            doc! {"_id" : poll_id},
+            doc! {
+                "$lookup": {
+                    "from": "options",
+                    "localField": "options",
+                    "foreignField": "_id",
+                    "as": "expanded_options"
+                }
+            },
+            doc! {
+                "$project": {
+                    "title": 1,
+                    "owner_id": 1,
+                    "expanded_options": 1
+                }
+            }
+        ];
+        let mut cursor = self.collection.aggregate(pipeline).await?;
+        
+        // Use try_next() to get the first result
+        let result = cursor.try_next().await?;
+
+        Ok(result)
     }
 }
