97fedaa41ebd37a0e8b7216e986127ab9e8da256
SATHVIK SVS <svssathvik77@gmail.com>
2025-01-24 11:45:03 +0530

    add anyhow to auth state repo

--- src/db/auth_state_repo.rs
+++ src/db/auth_state_repo.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use anyhow::Result;
 use log::{debug, error};
 use mongodb::{
     bson::doc,
@@ -40,48 +41,50 @@ impl AuthStateRepo {
         })
     }
 
-    pub async fn insert(
-        &self,
-        auth_state_entry: AuthState,
-    ) -> Result<InsertOneResult, mongodb::error::Error> {
+    pub async fn insert(&self, auth_state_entry: AuthState) -> Result<InsertOneResult> {
         let username = &auth_state_entry.username;
         let _auth_state_alread_exists = match self.is_exists(username).await {
             Ok(data) => {
                 if data {
+                    // cleanup any existing auth states
                     self.delete_by_username(username).await?;
                 } else {
                     ()
                 }
             }
             Err(e) => {
-                error!("Error deleting a duplicate record {:?}", e);
+                error!("Error deleting a duplicate auth state record {:?}", e);
             }
         };
-        let result = self.collection.insert_one(auth_state_entry).await;
+        let result = self
+            .collection
+            .insert_one(auth_state_entry)
+            .await
+            .map_err(|e| anyhow::Error::new(e));
         result
     }
 
-    pub async fn find_by_username(
-        &self,
-        username: &str,
-    ) -> Result<Option<AuthState>, mongodb::error::Error> {
+    pub async fn find_by_username(&self, username: &str) -> Result<Option<AuthState>> {
         let filter = doc! {"username": username};
-        let result = self.collection.find_one(filter).await;
+        let result = self.collection.find_one(filter).await.map_err(|e| {
+            error!("Error finding auth state by username {}", e);
+            anyhow::Error::new(e)
+        });
         debug!("found {:?}", result);
         result
     }
 
-    pub async fn is_exists(&self, username: &str) -> Result<bool, mongodb::error::Error> {
+    pub async fn is_exists(&self, username: &str) -> Result<bool> {
         let filter = doc! {"username": username};
         Ok(self.collection.find_one(filter).await?.is_some())
     }
 
-    pub async fn delete_by_username(
-        &self,
-        username: &str,
-    ) -> Result<DeleteResult, mongodb::error::Error> {
+    pub async fn delete_by_username(&self, username: &str) -> Result<DeleteResult> {
         let filter = doc! {"username": username};
-        let result = self.collection.delete_one(filter).await;
+        let result = self.collection.delete_one(filter).await.map_err(|e| {
+            error!("Error deleteing auth state by username {}", e);
+            anyhow::Error::new(e)
+        });
         result
     }
 }
